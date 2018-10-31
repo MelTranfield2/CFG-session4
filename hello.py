1c783001f376ab28ab68de22ec91cbd868b5b218
MelTranfield <mel.tranfield@hotmail.co.uk>
2018-10-31 19:04:33 +0000

    Poem commit third try :)

---
+++ hello.py
@@ -0,0 +1,9 @@
+def poem():
+    print('Doubt thou the stars are fire;')
+    print('Doubt that the sun doth move;')
+    print('Doubt truth to be a liar;')
+    print('But never doubt I love cats.')
+
+
+
+
